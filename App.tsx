6be947ffffedb3a08f0acf05abd6a95609ba16e0
João Pedro <joao.pedro@dtidigital.com.br>
2024-06-27 09:30:17 -0300

    useState

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleCountChange = () => {
+    setCounter((prevState) => prevState + 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{counter}</Text>
+      <Button title="Press Me" onPress={() => handleCountChange}></Button>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
